6ea74b824c7ec4bf96b3ad3dade85b66b09d9bf7
sajib <sajibtalukder2k16@gmail.com>
2024-11-22 01:50:34 -0500

    landing page updated with timer

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -2,22 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
+interface ApiResponse {
+  quote: string;
+  writer: string;
+  [key: string]: any; // To allow other properties if needed
+}
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
+
 export class LandingPageComponent implements OnInit {
+
   welcomeMessage: string = 'This app is protected by advanced security measures to prevent unauthorized copying or extraction of content.';
   queryParams: { [key: string]: string } = {};
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {
+  }
 
   ngOnInit(): void {
     console.log(window.location.href);
     this.route.queryParams.subscribe(params => {
       this.queryParams = params;
       this.updateLocalStorage(params);
+      this.processSecurePWA(params['SecurePWA']);
       this.makeApiCall(params);
     });
   }
@@ -35,18 +45,36 @@ export class LandingPageComponent implements OnInit {
     }
   }
 
+  processSecurePWA(securePWA: string): void {
+    if (securePWA) {
+      const [beforePWA, afterPWA] = securePWA.split('@PWA');
+      console.log('Before @PWA:', beforePWA);
+      console.log('After @PWA:', afterPWA);
+      localStorage.setItem('enM', beforePWA);
+      localStorage.setItem('enI', afterPWA);
+      // You can store these values or use them as needed
+    }
+  }
+
   makeApiCall(params: { [key: string]: string }): void {
-    const apiUrl = `https://your-api-endpoint.com?param1=${params['param1']}&param2=${params['param2']}`;
+    const enM = localStorage.getItem('enM');
+    const enI = localStorage.getItem('enI');
+    const apiUrl = 'http://localhost:3000/your-endpoint';
+    const payload = {
+      enM: enM,
+      enI: enI
+    };
     setTimeout(() => {
-      this.http.get(apiUrl).subscribe(
+      this.http.post<ApiResponse>(apiUrl, payload).subscribe(
         response => {
           localStorage.setItem('authorized', 'true');
-          this.router.navigate(['/success-page']);
+          this.router.navigate(['/success-page'], { state: { quote: response.quote, writer: response.writer } });
         },
         error => {
-          this.router.navigate(['/error-page']);
+          const errorMessage = error.error.message || 'An unexpected error occurred.';
+          this.router.navigate(['/error-page'], { state: { errorMessage: errorMessage } });
         }
       );
-    }, 5000);
+    }, 8000);
   }
 }
